d629d79cb1df74c5feb2af70e0864465e029667d
Diogo Valente <diogovalente@hotmail.com.br>
2022-05-30 17:18:47 -0300

    Project created

---
+++ src/test/java/br/com/foursys/fourcamp/fourstore/FourstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foursys.fourcamp.fourstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
